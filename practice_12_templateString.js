ff0d0f83be28d78cd57c7acf8b0bc07ec048a5fc
Nisar Shafi <nisarshafi1@gmail.com>
2023-05-22 15:38:02 -0400

    template12

--- practice_12_templateString.js
+++ practice_12_templateString.js
@@ -6,4 +6,25 @@ const age = 18;
 
 console.log("firstName is " + firstName);
 
-console.log(     );
+// ` backtick symbol for template string
+console.log(`firstNAme is ${firstName}`     );
+
+
+// // print out full name is firstNAme lastNAme and she is 18
+
+console.log(` full name is ${firstName} ${lastName} and she is ${age}`);
+
+const multiline = 
+` this is line 1
+this is line 2
+this is line 3`;
+console.log(multiline);
+
+
+console.log(`uppercase version is : ${multiline.toUpperCase() }`);
+
+
+const num1 = 10;
+const num2 = 20;
+
+console.log(`The result of adding ${num1} to ${num2} is ${num1+num2}`);
